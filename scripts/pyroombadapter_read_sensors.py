b13cb569b6d19fa5d8d9ce29f38112887cf571e9
Michael <mfs3ee@gmail.com>
2024-05-19 13:53:54 -0400

    Update to fork of ros2_bno055. Create scripts folder.

---
+++ scripts/pyroombadapter_read_sensors.py
@@ -0,0 +1,20 @@
+"""
+    Read Roomba sensors
+"""
+from time import sleep
+from pyroombaadapter import PyRoombaAdapter
+
+PORT = "/dev/ttyUSB0"
+adapter = PyRoombaAdapter(PORT)
+adapter.change_mode_to_passive()
+
+# Request sensor value manually
+print(f"Charging_state: {adapter.request_charging_state()}")
+print(f"Voltage: {adapter.request_voltage()}")
+print(f"Current: {adapter.request_current()}")
+print(f"Temperature: {adapter.request_temperature()}")
+print(f"Charge: {adapter.request_charge()}")
+print(f"Capacity: {adapter.request_capacity()}")
+print(f"OI Mode: {adapter.request_oi_mode()}")
+print(f"Distance: {adapter.request_distance()}")
+print(f"Angle: {adapter.request_angle()}")
