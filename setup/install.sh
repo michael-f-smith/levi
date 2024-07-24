0663cd2de13153992b231684aa615516bf9f9697
Michael <mfs3ee@gmail.com>
2024-07-23 22:13:56 -0400

    Add setup

---
+++ setup/install.sh
@@ -0,0 +1,62 @@
+# Install ROS2 Jazzy from src as detailed here: https://docs.ros.org/en/jazzy/Installation/Alternatives/Ubuntu-Development-Setup.html
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+
+#
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Install dev tools
+sudo apt update && sudo apt install -y \
+  python3-flake8-blind-except \
+  python3-flake8-class-newline \
+  python3-flake8-deprecated \
+  python3-mypy \
+  python3-pip \
+  python3-pytest \
+  python3-pytest-cov \
+  python3-pytest-mock \
+  python3-pytest-repeat \
+  python3-pytest-rerunfailures \
+  python3-pytest-runner \
+  python3-pytest-timeout \
+  ros-dev-tools
+
+# Build ROS2
+mkdir -p ~/ros2_jazzy/src
+cd ~/ros2_jazzy
+vcs import --input https://raw.githubusercontent.com/ros2/ros2/jazzy/ros2.repos src
+
+
+# Prior to installing new packages, make sure system is up to date
+sudo apt upgrade
+
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+
+
+# Build workspace
+cd ~/ros2_jazzy/
+colcon build --symlink-install
+
+
+
+
+
+
+
