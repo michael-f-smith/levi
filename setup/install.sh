b70d02ca4fdccd26a8b7ef44c764548cf07ec6ce
Michael <mfs3ee@gmail.com>
2024-07-24 22:33:35 -0400

    Add ros2-cross-compilation submodule. Add initial dockerfile.

--- setup/install.sh
+++ setup/install.sh
@@ -17,6 +17,8 @@ sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
+
+# Handle ros2.list issue on raspi
 if [$(. /etc/os-release && echo $UBUNTU_CODENAME) ]
 then
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
